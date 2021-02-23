5d1e8b753503c8c624d7a978ada54c7decff9f39
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-23 13:53:03 -0800

    added all assets and imports after splitting with boxy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 // Imports
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import falcon1 from './assets/falcon1.svg';
+import falcon9v11a from './assets/falcon911a.svg';
+import falcon9v11b from './assets/falcon911b.svg';
+import falcon9v11c from './assets/falcon911c.svg';
+import falcon9v12a from './assets/falcon912a.svg';
+import falcon9v12b from './assets/falcon912b.svg';
+import falcon9v12c from './assets/falcon912c.svg';
+import falcon9block5a from './assets/falcon9block5a.svg';
+import falcon9block5b from './assets/falcon9block5b.svg';
+import falcon9block5c from './assets/falcon9block5c.svg';
+import falconheavy from './assets/falconheavy.svg';
+import falconheavyblock5 from './assets/falconheavyblock5.svg';
 
 import './App.css';
 
@@ -86,6 +98,44 @@ function App({}: AppProps) {
                         {launch.success === null
                           ? 'Not available'
                           : launch.success.toString()}
+                      </div>
+                      <div className="rocket-display">
+                        <div>
+                          <img src={falcon1}></img>
+                        </div>
+                        <div>
+                          <img src={falcon9v11a}></img>
+                        </div>
+                        <div>
+                          <img src={falcon9v11b}></img>
+                        </div>
+                        <div>
+                          <img src={falcon9v11c}></img>
+                        </div>
+                        <div>
+                          <img src={falcon9v12a}></img>
+                        </div>
+                        <div>
+                          <img src={falcon9v12b}></img>
+                        </div>
+                        <div>
+                          <img src={falcon9v12c}></img>
+                        </div>
+                        <div>
+                          <img src={falcon9block5a}></img>
+                        </div>
+                        <div>
+                          <img src={falcon9block5b}></img>
+                        </div>
+                        <div>
+                          <img src={falcon9block5c}></img>
+                        </div>
+                        <div>
+                          <img src={falconheavy}></img>
+                        </div>
+                        <div>
+                          <img src={falconheavyblock5}></img>
+                        </div>
                       </div>
                     </Col>
                   </Row>
