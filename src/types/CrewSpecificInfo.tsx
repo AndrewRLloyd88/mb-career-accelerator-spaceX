b53ed46d9fa9afc519d3f18a55c57dac035c2b1f
joecotterchio <joecotterchio@gmail.com>
2021-02-24 15:06:53 -0500

    crewinfo

---
+++ src/types/CrewSpecificInfo.tsx
@@ -0,0 +1,9 @@
+export interface CrewSpecificInfo {
+    name: string,
+    agency: string,
+    image: string,
+    wikipedia: string,
+    launches: string,
+    status: string,
+    id: string,
+}
