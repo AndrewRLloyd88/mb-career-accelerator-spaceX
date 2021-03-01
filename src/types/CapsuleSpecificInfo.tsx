7c206348a208552a2a8d8bba8c00cbcc486882d0
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-01 10:28:22 -0800

    complete Capsule Definition

---
+++ src/types/CapsuleSpecificInfo.tsx
@@ -0,0 +1,11 @@
+export interface CapsuleSpecificInfo {
+  reuse_count: number;
+  water_landings: number;
+  land_landings: number;
+  last_update: string;
+  launches: string[];
+  serial: string;
+  status: string;
+  type: string;
+  id: string;
+}
