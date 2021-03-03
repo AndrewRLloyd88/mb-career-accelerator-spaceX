1623be7740ac09402f9a35247ca6c18f01511ef7
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-02 18:41:07 -0800

    code cleanup for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// Import images
-import falcon1 from './assets/falcon1.svg';
-import falcon9v11a from './assets/falcon911a.svg';
-import falcon9v11b from './assets/falcon911b.svg';
-import falcon9v11c from './assets/falcon911c.svg';
-import falcon9v12a from './assets/falcon912a.svg';
-import falcon9v12b from './assets/falcon912b.svg';
-import falcon9v12c from './assets/falcon912c.svg';
-import falcon9block5a from './assets/falcon9block5a.svg';
-import falcon9block5b from './assets/falcon9block5b.svg';
-import falcon9block5c from './assets/falcon9block5c.svg';
-import falconheavy from './assets/falconheavy.svg';
-import falconheavyblock5 from './assets/falconheavyblock5.svg';
-
 import './App.css';
 
 // Import types
@@ -51,6 +37,7 @@ function App({}: AppProps) {
             $lte: new Date(),
           },
         },
+        //options needed for images
         options: {
           populate: [
             {
@@ -67,7 +54,7 @@ function App({}: AppProps) {
         },
       })
       .then((response) => {
-        console.log(response.data.docs);
+        // console.log(response.data.docs);
         setSlides(response.data.docs);
       })
       .catch((error) => console.log('API error: ', error));
@@ -124,6 +111,7 @@ function App({}: AppProps) {
                           xs={{ span: 12, order: 1 }}
                           md={{ span: 3, order: 2 }}
                         >
+                          {/* Rocket Image */}
                           <RocketImage rocketName={launch.rocket.name} />
                         </Col>
                       </Row>
